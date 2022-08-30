f7b644f950f4172321b3164a7751d455d0ebf095
shalant <doug.rosenberg@gmail.com>
2022-08-30 13:41:17 -0500

    got a decent start, navbar works, added footer

--- TPFE/src/app/app-routing.module.ts
+++ TPFE/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DevicesComponent } from './devices/devices.component';
+import { PlansComponent } from './plans/plans.component';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'index', pathMatch: 'full' },
+  { path: 'Users', component: UsersComponent },
+  { path: 'Devices', component: DevicesComponent },
+  { path: 'Plans', component: PlansComponent },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TPFE/src/app/app.module.ts
+++ TPFE/src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { PlansComponent } from './plans/plans.component';
 import { DevicesComponent } from './devices/devices.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { UsersModule } from './users/users.module';
+import { DevicesModule } from './devices/devices.module';
+import { PlansModule } from './plans/plans.module';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent,
-    PlansComponent,
-    DevicesComponent
+    CardComponent,
+    // UsersComponent,
+    // PlansComponent,
+    // DevicesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    UsersModule,
+    DevicesModule,
+    PlansModule
   ],
   providers: [],
   bootstrap: [AppComponent]
