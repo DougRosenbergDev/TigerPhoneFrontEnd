42c6f4784fd3b6228b90432468a9a424908a08c5
shalant <doug.rosenberg@gmail.com>
2022-08-31 21:23:00 -0500

    double check

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DevicesComponent } from './devices/devices.component';
 import { PlansComponent } from './plans/plans.component';
+import { IndexComponent } from './users/index/index.component';
 import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'index', pathMatch: 'full' },
-  { path: 'Users', component: UsersComponent },
+  { path: 'Users', component: IndexComponent },
   { path: 'Devices', component: DevicesComponent },
   { path: 'Plans', component: PlansComponent },
 
