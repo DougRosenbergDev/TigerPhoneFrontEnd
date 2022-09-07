11a8cd71bd544421270c510e692a917e4c007482
shalant <doug.rosenberg@gmail.com>
2022-09-06 23:18:57 -0500

    minimal styling

--- src/app/users/index/index.component.ts
+++ src/app/users/index/index.component.ts
@@ -4,6 +4,7 @@ import { UsersService } from '../users.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Plan } from 'src/app/plans/plan';
 import { Device } from 'src/app/devices/device';
+import { DevicesService } from 'src/app/devices/devices.service';
 
 @Component({
   selector: 'app-index',
@@ -14,13 +15,14 @@ export class IndexComponent implements OnInit {
   id!: number;
   users?: User[];
   plans?: Plan[];
-  devices: Device[] = [];
+  devices?: Device[];
   // planName: 
   // id!: number;
 
   // ask angular to inject musicService
   constructor(
     private userService: UsersService,
+    private devicesService: DevicesService,
     private rout: ActivatedRoute,
     private router: Router  
   ) {}
@@ -28,6 +30,7 @@ export class IndexComponent implements OnInit {
   ngOnInit(): void {
     this.retrieveUsers();
     this.retrievePlans();
+    this.retrieveDevices();
   }
 
   // retrievePlans(): void {
@@ -50,6 +53,13 @@ export class IndexComponent implements OnInit {
   retrieveDevices(): void {
     this.userService.getUserPlans().subscribe(plans => this.plans = plans);
     // console.log(this.plans[0]);
+  }
+
+  deleteDevice(id:number) {
+    this.devicesService.deleteDevice(id).subscribe(res => {
+      // this.devices = this.devices.filter(item => item.id !==id);
+      console.log("Device deleted successfully")
+    })
   }
 
 }
